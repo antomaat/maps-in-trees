aba9d3b25384b16fcb4834c603896744ebe07dec
anto <anto>
2024-01-19 22:13:25 +0200

    init loading dir

---
+++ go.mod
@@ -0,0 +1,8 @@
+module app
+
+go 1.16
+
+require (
+	github.com/veandco/go-sdl2 v0.4.36
+	golang.org/x/tools/gopls v0.14.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+    arguments := os.Args[1:]
+    if (len(arguments) == 0) {
+        fmt.Println("missing directory path \n")
+        os.Exit(1) 
+    }
+
+    dir := arguments[0]
+
+    fmt.Println("Hello World")
+    items, _ := os.ReadDir(dir)
+
+    if (len(items) == 0) {
+        fmt.Printf("directory %s empty or missing \n", dir)
+        os.Exit(1)
+    }
+    for i := 0; i < len(items); i++ {
+        fmt.Println(items[i].Name())
+    }
+}
+
