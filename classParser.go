93bc1b5dff017aeecfa561353d0d82e86fdc9b14
anto <anto>
2024-03-04 23:09:58 +0200

    BROKEN update parser related stuff

--- classParser.go
+++ classParser.go
@@ -6,6 +6,31 @@ import (
 	"fmt"
 )
 
+type FileParser struct {
+    index int64 
+    file []byte
+}
+
+func (fp FileParser) jumpByte() FileParser {
+    fp.index += 1
+    return fp
+}
+
+func (fp *FileParser) jumpBytes(count int64) *FileParser {
+    fp.index += count
+    return fp
+}
+
+func (fp FileParser) readValue(bytes int64) []byte {
+    return fp.file[fp.index: fp.index + bytes]
+}
+
+func (fp *FileParser) readValueAndUpdateIndexBy(bytes int64) []byte {
+    value := fp.file[fp.index: fp.index + bytes]
+    fp.index += bytes
+    return value
+}
+
 type ConstantPool struct {
     tag int
     constantUtf8 ConstantUtf8
@@ -61,13 +86,13 @@ type ConstantFloatInfo struct {
 }
 
 type ConstantLongInfo struct {
-    highBytes int64
-    lowBytes int64
+    highBytes uint64
+    lowBytes uint64
 }
 
 type ConstantDoubleInfo struct {
-    highBytes int64
-    lowBytes int64
+    highBytes uint64
+    lowBytes uint64
 }
 
 type ConstantNameAndTypeInfo struct {
@@ -90,16 +115,21 @@ type ConstantInvokeDynamicInfo struct {
 }
 
 func ParseFileInfo(file []byte) {
+    fileParser := FileParser {
+        index: 0,
+        file: file,
+    }
+
     fmt.Println("parse file info")
     index := 0;
-    if len(file) > 0 {
-        if !isValidFile(file) {
+    if len(fileParser.file) > 0 {
+        if !isValidFile(fileParser) {
             return
         }
 
+        fileParser.readValueAndUpdateIndexBy(8)
         // skip the magic number, minor and major versions
-        index += 8
-        constantPoolCount := binary.BigEndian.Uint16(file[index:index + 2])
+        constantPoolCount := binary.BigEndian.Uint16(fileParser.readValueAndUpdateIndexBy(2))
         fmt.Printf("constant pool count %d \n", constantPoolCount)
         index += int(constantPoolCount)
 
@@ -110,49 +140,85 @@ func ParseFileInfo(file []byte) {
                     tag: int(file[index]),
                 }
             index++;
-            updateConstantPoolItem(&poolItem, file, index)
+            updateConstantPoolItem(&poolItem, &fileParser)
             constantPool = append(constantPool, poolItem)
+            printConstantPool(poolItem)
         }
-        fmt.Println(constantPool)
     }
 }
 
-func updateConstantPoolItem(poolItem *ConstantPool, bytes []byte, index int) {
+func updateConstantPoolItem(poolItem *ConstantPool, fileParser *FileParser) {
     switch tag := poolItem.tag; tag {
     case 1:
-        length := binary.BigEndian.Uint16(bytes[index:index + 2]) 
-        index += 2;
-        string := bytes[index: index + int(length)]
-        index += int(length)
+        length := binary.BigEndian.Uint16(fileParser.readValueAndUpdateIndexBy(2)) 
+        string := fileParser.readValueAndUpdateIndexBy(int64(length))
         poolItem.constantUtf8 = ConstantUtf8{
             length: int(length),
             bytes: string,
         }
     case 3:
-        fmt.Println("3 or 4")
-        numb := binary.BigEndian.Uint32(bytes[index: index + 4])
-        index += 4;
+        numb := binary.BigEndian.Uint32(fileParser.readValueAndUpdateIndexBy(4))
         poolItem.constantIntegerInfo = ConstantIntegerInfo{
             bytes: numb,
         }
     case 4:
-        numb := binary.BigEndian.Uint32(bytes[index: index + 4])
-        index += 4;
+        numb := binary.BigEndian.Uint32(fileParser.readValueAndUpdateIndexBy(4))
         poolItem.constantFloatInfo = ConstantFloatInfo {
             bytes: numb,
         }
+    case 5:
+        high := binary.BigEndian.Uint64(fileParser.readValueAndUpdateIndexBy(4))
+        low := binary.BigEndian.Uint64(fileParser.readValueAndUpdateIndexBy(4))
+        poolItem.constantLongInfo = ConstantLongInfo {
+            highBytes: high,
+            lowBytes: low,
+        }
+    case 6:
+        high := binary.BigEndian.Uint64(fileParser.readValueAndUpdateIndexBy(4))
+        low := binary.BigEndian.Uint64(fileParser.readValueAndUpdateIndexBy(4))
+        poolItem.constantDoubleInfo = ConstantDoubleInfo {
+            highBytes: high,
+            lowBytes: low,
+        }
+    case 7:
+        nameIndex := binary.BigEndian.Uint64(fileParser.readValueAndUpdateIndexBy(2))    
+
+        poolItem.classInfo = ClassInfo{
+            nameIndex: int(nameIndex),
+        }
 
     default:
-        fmt.Println("missing tag")
+        fmt.Printf("missing tag %d \n", tag)
     }
 
 }
 
-func isValidFile(file []byte) bool {
-    magic := file[:4]
+func isValidFile(file FileParser) bool {
+    magic := file.readValue(4)
     str := hex.EncodeToString(magic)
     if (str != "cafebabe") {
         return false
     }
     return true
+}
+
+func printConstantPool(cp ConstantPool) {
+    fmt.Println("--- Constant Pool ---")
+    fmt.Printf("tag %d \n", cp.tag)
+    switch tag := cp.tag; tag {
+    case 1:
+        fmt.Printf("string: %s \n", cp.constantUtf8.bytes)
+    case 3:
+        fmt.Printf("integer: %d \n", cp.constantIntegerInfo.bytes)
+    case 4:
+        fmt.Printf("float: %d \n", cp.constantFloatInfo.bytes)
+    case 5:
+        fmt.Printf("long low: %d \n", cp.constantLongInfo.lowBytes)
+        fmt.Printf("long high: %d \n", cp.constantLongInfo.highBytes)
+    case 6:
+        fmt.Printf("double low: %d \n", cp.constantDoubleInfo.lowBytes)
+        fmt.Printf("double high: %d \n", cp.constantDoubleInfo.highBytes)
+    case 7:
+        fmt.Printf("class %d \n", cp.classInfo.nameIndex)
+    }
 }
